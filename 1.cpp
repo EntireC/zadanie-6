82f1582a0f89d893071829205051c06afb07c6cb
EntireC <91829056+EntireC@users.noreply.github.com>
2021-12-19 23:25:56 +0300

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -4,9 +4,9 @@
 using namespace std;
 
 const int sizea = 5, sizeb = 5;
-int arr[sizea][sizeb];
 
-void fillrand(int arr[sizea][sizeb]) {
+
+void fillrand(int** arr) {
 	srand(time(NULL));
 	for (int a = 0; a < sizea; a++)
 		for (int b = 0; b < sizeb; b++) {
@@ -14,17 +14,16 @@ void fillrand(int arr[sizea][sizeb]) {
 		}
 }
 
-template <size_t rows, size_t cols>
-void print(int(&arr)[rows][cols]) {
-	for (int a = 0; a < rows; a++) {
-		for (int b = 0; b < cols; b++) {
+void print(int** arr, int x, int y) {
+	for (int a = 0; a < x; a++) {
+		for (int b = 0; b < y; b++) {
 			cout << arr[a][b] << ' ';
 		}
 		cout << endl;
 	}
 }
 
-void erase(int arr[sizea][sizeb], int newarr[sizea - 1][sizeb - 1]) {
+void erase(int** arr, int** newarr) {
 	int x = 0, y = 0, min = arr[0][0];
 	for (int a = 0; a < sizea; a++) {
 		for (int b = 0; b < sizeb; b++) {
@@ -35,27 +34,38 @@ void erase(int arr[sizea][sizeb], int newarr[sizea - 1][sizeb - 1]) {
 			}
 		}
 	}
-	for (int a = x; a < sizea - 1; a++) {
-		for (int b = 0; b < sizeb; b++) {
-			newarr[a][b] = newarr[a + 1][b];
-		}
-	}
-	for (int b = y; b < sizeb - 1; b++) {
-		for (int a = 0; a < sizea; a++) {
-			newarr[a][b] = newarr[a][b + 1];
+	for (int a = 0; a < sizea - 1; a++) {
+		for (int b = 0; b < sizeb - 1; b++) {
+			if (a<x && b<y) newarr[a][b] = arr[a][b];
+			else if (a>=x && b<y) newarr[a][b] = arr[a+1][b];
+			else if (a<x && b>=y) newarr[a][b] = arr[a][b+1];
+			else if (a >= x && b >= y) newarr[a][b] = arr[a+1][b + 1];
 		}
 	}
 }
 
 int main()
 {
+	int **arr;
+	arr = new int *[sizea];
+	for (int i = 0; i < sizea; i++)
+		arr[i] = new int[sizeb];
+
+	int **newarr;
+	newarr = new int *[sizea-1];
+	for (int i = 0; i < sizea-1; i++)
+		newarr[i] = new int[sizeb-1];
+
 	fillrand(arr);
-	print(arr);
-	int newarr[sizea - 1][sizeb - 1];
+	print(arr, sizea, sizeb);
 	erase(arr, newarr);
+
+	for (int i = 0; i < sizea; i++)
+		delete[] arr[i];
 	delete[] arr;
+
 	cout << endl;
-	print(newarr);
+	print(newarr, sizea-1, sizeb-1);
 	cout << endl;
 	return 0;
 }
