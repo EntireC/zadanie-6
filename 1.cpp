afc5c7aec05c324d6fde0dac59486b58022aa693
EntireC <91829056+EntireC@users.noreply.github.com>
2021-12-19 22:48:56 +0300

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -8,31 +8,23 @@ int arr[sizea][sizeb];
 
 void fillrand(int arr[sizea][sizeb]) {
 	srand(time(NULL));
-	for (int a = 0; a < sizea; a++) 
+	for (int a = 0; a < sizea; a++)
 		for (int b = 0; b < sizeb; b++) {
-			arr[a][b] = rand()%100;
+			arr[a][b] = rand() % 100;
 		}
 }
 
-void print(int arr[sizea][sizeb]) {
-	for (int a = 0; a < sizea; a++) {
-		for (int b = 0; b < sizeb; b++) {
+template <size_t rows, size_t cols>
+void print(int(&arr)[rows][cols]) {
+	for (int a = 0; a < rows; a++) {
+		for (int b = 0; b < cols; b++) {
 			cout << arr[a][b] << ' ';
 		}
 		cout << endl;
 	}
 }
 
-void nprint(int arr[sizea][sizeb]) {
-	for (int a = 0; a < sizea-1; a++) {
-		for (int b = 0; b < sizeb-1; b++) {
-			cout << arr[a][b] << ' ';
-		}
-		cout << endl;
-	}
-}
-
-void erase(int arr[sizea][sizeb]) {
+void erase(int arr[sizea][sizeb], int newarr[sizea - 1][sizeb - 1]) {
 	int x = 0, y = 0, min = arr[0][0];
 	for (int a = 0; a < sizea; a++) {
 		for (int b = 0; b < sizeb; b++) {
@@ -43,27 +35,27 @@ void erase(int arr[sizea][sizeb]) {
 			}
 		}
 	}
-	for (int a = x; a < sizea-1; a++) {
+	for (int a = x; a < sizea - 1; a++) {
 		for (int b = 0; b < sizeb; b++) {
-			arr[a][b] = arr[a+1][b];
+			newarr[a][b] = newarr[a + 1][b];
 		}
 	}
-	for (int b = y; b < sizeb-1; b++) {
+	for (int b = y; b < sizeb - 1; b++) {
 		for (int a = 0; a < sizea; a++) {
-			arr[a][b] = arr[a][b+1];
+			newarr[a][b] = newarr[a][b + 1];
 		}
 	}
-	for (int a = 0; a < sizea; a++) arr[a][sizeb-1] = '\0';
-	for (int b = 0; b < sizeb; b++) arr[sizea-1][b] = '\0';
 }
 
 int main()
 {
 	fillrand(arr);
 	print(arr);
-	erase(arr);
+	int newarr[sizea - 1][sizeb - 1];
+	erase(arr, newarr);
+	delete[] arr;
 	cout << endl;
-	nprint(arr);
+	print(newarr);
 	cout << endl;
-    return 0;
+	return 0;
 }
